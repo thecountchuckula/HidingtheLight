f51a94d937d74d46a71e3c236a3c41047f771a47
thecountchuckula <rainsdaddy@gmail.com>
2013-08-17 05:41:41 -0700

    oops
    had copied the wrong file

--- Common/tcc.HidingtheLight/WandLighting.java
+++ Common/tcc.HidingtheLight/WandLighting.java
@@ -1,71 +1,100 @@
 package tcc.HidingtheLight;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.EnumHelper;
-import net.minecraftforge.common.MinecraftForge;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.world.World;
 
-public class ModInfo {
 
-	public static final String NAME = "Hiding the Light";
-	public static final String MOD_ID = "htl";
-	public static final String version = "v1.1-1.6.2";
+public class WandLighting extends Item {
 
-	public static Block glowair;
-	public static Item wandlighting;
-		
-	public static final ItemStack glow = new ItemStack(Item.glowstone, 1, 0);
-	public static final ItemStack stick = new ItemStack(Item.stick, 1, 0);
-	public static final ItemStack gnugget = new ItemStack(Item.goldNugget, 1, 0);
-	public static final ItemStack hblaze = new ItemStack(Item.blazeRod, 1, 0);
-	public static final ItemStack hglow = new ItemStack(Block.glowStone, 1, 0);
-	public static final ItemStack hgold = new ItemStack(Item.ingotGold, 1, 0);
-	
-	int glowairID;
-	int wandlightingID;
-	
-	
-	// Block Information Section  - Thanks to travi1997 on MinecraftForums.com for helping me figure out my BlockIDs problems.
-	public static void initItems(){
-		wandlighting = new WandLighting(ModMain.wandlightingID);
-	}
-	public static void initBlock(){
-		
-		glowair = new GlowAir(ModMain.glowairID);
-		MinecraftForge.setBlockHarvestLevel(glowair, null, 0);
-		GameRegistry.registerBlock(glowair, "glowair");
-		
-	}
-	
-	public static void languageRegistry() {
-		LanguageRegistry.addName(glowair, "Glowing Air");
-		LanguageRegistry.addName(wandlighting, "Wand of Hidden Light");
-	}
-	
-	public static void crafting() {
-		if(ModMain.enableHardcore == false) {
-        	GameRegistry.addRecipe(new ItemStack(wandlighting, 1),
-    				"  g", 
-    				" s ", 
-    				"n  ",
-    				'g', glow,
-    				's', stick,
-    				'n', gnugget);
+	public WandLighting(int par1) {
+		super(par1);
+		// TODO Auto-generated constructor stub
+		this.setMaxStackSize(1);
+		if(ModMain.lastsLonger == false) {
+			this.setMaxDamage(64);
 		}
-		if(ModMain.enableHardcore == true) {
-			GameRegistry.addRecipe(new ItemStack(wandlighting, 1), 
-					"  g", 
-					" s ", 
-					"n  ",
-					'g', hglow,
-					's', hblaze,
-					'n', hgold);
+		if(ModMain.lastsLonger == true) {
+			this.setMaxDamage(256);
 		}
+		this.setCreativeTab(CreativeTabs.tabTools);
+		this.setUnlocalizedName("wandLighting");
+	}
+	
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+
+	public void registerIcons(IconRegister par1IconRegister) {
+		if(ModMain.newIcon == false) {
+			itemIcon = par1IconRegister.registerIcon(ModInfo.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(5));
+		}
+		if(ModMain.newIcon == true) {
+			itemIcon = par1IconRegister.registerIcon(ModInfo.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(5) + "_hard");
+		}
+	}
+	
+	
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+    {
+        if (par7 == 0)
+        {
+            --par5;
+        }
+
+        if (par7 == 1)
+        {
+            ++par5;
+        }
+
+        if (par7 == 2)
+        {
+            --par6;
+        }
+
+        if (par7 == 3)
+        {
+            ++par6;
+        }
+
+        if (par7 == 4)
+        {
+            --par4;
+        }
+
+        if (par7 == 5)
+        {
+            ++par4;
+        }
+
+        if (!par2EntityPlayer.canPlayerEdit(par4, par5, par6, par7, par1ItemStack))
+        {
+            return false;
+        }
+        else
+        {
+            if (par3World.isAirBlock(par4, par5, par6))
+            {
+                par3World.playSoundEffect((double)par4 + 0.5D, (double)par5 + 0.5D, (double)par6 + 0.5D, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
+                par3World.setBlock(par4, par5, par6, ModInfo.glowair.blockID);
+            }
+
+            par1ItemStack.damageItem(1, par2EntityPlayer);
+            return true;
+        }		
+        
 	}
 }
+
+
+
+
+
+
